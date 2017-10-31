369920b1c79e94d30923818878fc9ab3e37a005d
pthiep <phantanhiepnt@gmail.com>
2017-10-31 14:22:59 +0700

    Tạo class lưu dữ liệu

---
+++ Hethongquanlyquanan/Boquanquanly/NewObject/DTUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boquanquanly.NewObject
+{
+    public class DTUser
+    {
+        public static string ID_USER = "";
+    }
+}
