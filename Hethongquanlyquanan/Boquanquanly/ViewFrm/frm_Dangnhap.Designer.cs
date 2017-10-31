44ba3140cd7a20204669e30dfe71165078df8b2e
pthiep <phantanhiepnt@gmail.com>
2017-10-31 13:16:30 +0700

    Update

--- Hethongquanlyquanan/Boquanquanly/ViewFrm/frm_Dangnhap.Designer.cs
+++ Hethongquanlyquanan/Boquanquanly/ViewFrm/frm_Dangnhap.Designer.cs
@@ -31,8 +31,8 @@
             this.label1 = new System.Windows.Forms.Label();
             this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
             this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
-            this.metroTextBox1 = new MetroFramework.Controls.MetroTextBox();
-            this.metroTextBox2 = new MetroFramework.Controls.MetroTextBox();
+            this.txtBox_Username = new MetroFramework.Controls.MetroTextBox();
+            this.txtBox_Password = new MetroFramework.Controls.MetroTextBox();
             this.metroButton1 = new MetroFramework.Controls.MetroButton();
             this.SuspendLayout();
             // 
@@ -63,19 +63,21 @@
             this.metroLabel2.TabIndex = 10;
             this.metroLabel2.Text = "Mật khẩu:";
             // 
-            // metroTextBox1
+            // txtBox_Username
             // 
-            this.metroTextBox1.Location = new System.Drawing.Point(129, 70);
-            this.metroTextBox1.Name = "metroTextBox1";
-            this.metroTextBox1.Size = new System.Drawing.Size(135, 23);
-            this.metroTextBox1.TabIndex = 11;
+            this.txtBox_Username.Location = new System.Drawing.Point(129, 70);
+            this.txtBox_Username.Name = "txtBox_Username";
+            this.txtBox_Username.Size = new System.Drawing.Size(135, 23);
+            this.txtBox_Username.TabIndex = 11;
+            this.txtBox_Username.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtBox_Username_KeyPress);
             // 
-            // metroTextBox2
+            // txtBox_Password
             // 
-            this.metroTextBox2.Location = new System.Drawing.Point(129, 107);
-            this.metroTextBox2.Name = "metroTextBox2";
-            this.metroTextBox2.Size = new System.Drawing.Size(135, 23);
-            this.metroTextBox2.TabIndex = 12;
+            this.txtBox_Password.Location = new System.Drawing.Point(129, 107);
+            this.txtBox_Password.Name = "txtBox_Password";
+            this.txtBox_Password.Size = new System.Drawing.Size(135, 23);
+            this.txtBox_Password.TabIndex = 12;
+            this.txtBox_Password.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtBox_Password_KeyPress);
             // 
             // metroButton1
             // 
@@ -84,6 +86,7 @@
             this.metroButton1.Size = new System.Drawing.Size(75, 23);
             this.metroButton1.TabIndex = 13;
             this.metroButton1.Text = "Đăng nhập";
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
             // 
             // frm_Dangnhap
             // 
@@ -91,8 +94,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(287, 228);
             this.Controls.Add(this.metroButton1);
-            this.Controls.Add(this.metroTextBox2);
-            this.Controls.Add(this.metroTextBox1);
+            this.Controls.Add(this.txtBox_Password);
+            this.Controls.Add(this.txtBox_Username);
             this.Controls.Add(this.metroLabel2);
             this.Controls.Add(this.metroLabel1);
             this.Controls.Add(this.label1);
@@ -108,8 +111,8 @@
         private System.Windows.Forms.Label label1;
         private MetroFramework.Controls.MetroLabel metroLabel1;
         private MetroFramework.Controls.MetroLabel metroLabel2;
-        private MetroFramework.Controls.MetroTextBox metroTextBox1;
-        private MetroFramework.Controls.MetroTextBox metroTextBox2;
+        private MetroFramework.Controls.MetroTextBox txtBox_Username;
+        private MetroFramework.Controls.MetroTextBox txtBox_Password;
         private MetroFramework.Controls.MetroButton metroButton1;
     }
 }
