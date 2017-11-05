b18040dee85db921e0cd118381ee53c283c2fc0d
KimHong1997 <31797803+KimHong1997@users.noreply.github.com>
2017-11-06 02:06:00 +0700

    Tao class

---
+++ DatHang.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gioithieuvadathang.DTO
+{
+    class DatHang
+    {
+        public class DatHang
+        {
+            public string MaKH { get; set; }
+            public HoaDon ChiTietHoaDon { get; set; }
+            public Date ThoiGianGiaoHang { get; set; }
+            public string DiaChi { get; set; }
+            public string SDT { get; set; }
+            public double TongTien { get; set; }
+        }
+        public string MaKH { get; set; }
+        public HoaDon ChiTietHoaDon { get; set; }
+        public Date ThoiGianGiaoHang { get; set; }
+        public string DiaChi { get; set; }
+        public string SDT { get; set; }
+        public double TongTien { get; set; }
+
+    }
+}
+
+   
