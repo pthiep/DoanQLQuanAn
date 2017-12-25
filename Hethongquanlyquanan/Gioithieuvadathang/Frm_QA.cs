5dcff68cf2b38e8354d6a3e328f7c2831c85d20f
KimHong1997 <kimhongkitty@gmail.com>
2017-12-25 13:31:24 +0700

    Vẽ lại form Giới thiệu và đặt hàng

--- Hethongquanlyquanan/Gioithieuvadathang/Frm_QA.cs
+++ Hethongquanlyquanan/Gioithieuvadathang/Frm_QA.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Text;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DevExpress.XtraBars;
 
 namespace Gioithieuvadathang
 {
-    public partial class Form1 : Form
+    public partial class Frm_QA : DevExpress.XtraBars.Ribbon.RibbonForm
     {
-        public Form1()
+        public Frm_QA()
         {
             InitializeComponent();
         }
